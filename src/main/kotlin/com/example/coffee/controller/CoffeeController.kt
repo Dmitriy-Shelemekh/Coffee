e01147ef090d57e2959fa8280ce76aa3522cfc34
shelemekh_dd <shelemekh_dd@ursip.ru>
2024-05-28 09:04:03 +0300

    Откат транзакций, корутины и трейсы

--- src/main/kotlin/com/example/coffee/controller/CoffeeController.kt
+++ src/main/kotlin/com/example/coffee/controller/CoffeeController.kt
@@ -3,8 +3,8 @@ package com.example.coffee.controller
 import com.example.coffee.model.dto.CoffeeDto
 import com.example.coffee.service.CoffeeService
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.slf4j.MDCContext
+import kotlinx.coroutines.withContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,7 +18,7 @@ class CoffeeController(
     private val service: CoffeeService
 ) {
     @PostMapping("/transaction")
-    suspend fun testTransaction(@RequestBody dto: CoffeeDto): ResponseEntity<Any> = runBlocking(Dispatchers.IO + MDCContext()) {
+    suspend fun testTransaction(@RequestBody dto: CoffeeDto): ResponseEntity<Any> = withContext(Dispatchers.IO + MDCContext()) {
         service.testTransaction(dto)
         ResponseEntity(HttpStatus.OK)
     }
